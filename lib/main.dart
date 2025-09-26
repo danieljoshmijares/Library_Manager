ecd1171d6444e9af6e87e2e6a85066d8b75e74e5
danieljoshmijares <mda1588@dlsud.edu.ph>
2025-09-27 02:29:49 +0800

    Renamed the app name in main.dart, AndroidManifest.xml, Info.plist, index.html, and main.cpp. I also added a short description. I updated launch.json.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Library Transaction System',
+      title: 'Library Manager',
       // UI MODERNIZATION: Updated theme with blue color scheme
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
